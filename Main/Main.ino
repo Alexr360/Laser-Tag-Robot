1f3070c18dc9575672a1d5f282b8884ea4fc80ee
Alexr360 <70153720+Alexr360@users.noreply.github.com>
2022-09-06 17:35:43 -0600

    Added Multiple LEDs to Indicate Lives

--- Main/Main.ino
+++ Main/Main.ino
@@ -14,9 +14,12 @@ Servo
 Reciever
   Pin
   
-LED
-  Pin 5
- */
+LEDs
+  #1:5
+  #2:6
+  #3:7
+
+*/
 
 //Define Components & Pins
 int laserEmitterPin = 3;
@@ -24,11 +27,13 @@ int laserRecier = 4;
 Servo steeringServo;
 
 //LEDs
-int IndicatorLED = 5;
+int IndicatorLED1 = 5;
+int IndicatorLED2 = 6;
+int IndicatorLED3 = 7;
 
 //Startup Values
-int startSteeringPos = 90;
-int lives = 3;
+int startSteeringPos = 90; //Servo can be set from 0-180 so 90 is the middle
+int lives = 3; // Number of Lives at the start
 
 void setup() {
   //Laser Setup
@@ -37,27 +42,44 @@ void setup() {
 
   //Steering Servo Setup
   steeringServo.attach(2);
-  steeringServo.write(startSteeringPos);
 
-  //LED Output
-  pinMode(IndicatorLED, OUTPUT);
+  //LED Indicators
+  pinMode(IndicatorLED1, OUTPUT);
+  pinMode(IndicatorLED2, OUTPUT);
+  pinMode(IndicatorLED3, OUTPUT);
+
+  //Init Functions
+  steeringServo.write(startSteeringPos);  //Set steering to forwards
+  digitalWrite(IndicatorLED1, HIGH);      //Enable LED Indicator 1
+  digitalWrite(IndicatorLED2, HIGH);      //Enable LED Indicator 2
+  digitalWrite(IndicatorLED3, HIGH);      //Enable LED Indicator 3
 }
 
 void loop() {
-  int laserState = digitalRead(laserRecier);
-
-  if (laserState == true) {
-    digitalWrite(IndicatorLED, HIGH);
-    delay(500);
-    digitalWrite(IndicatorLED, LOW);
-    //Hit Detected
-    lives = lives-1;
-    if (lives < 1) {
-      digitalWrite(IndicatorLED, HIGH);
-      //Robot is Dead
-      digitalWrite(laserEmitterPin, LOW);
-    }
+  if (lives < 1) {
+    //Robot is Dead
+    digitalWrite(IndicatorLED1, LOW);     //Disables LED Indicator 1
+    digitalWrite(IndicatorLED2, LOW);     //Disables LED Indicator 2
+    digitalWrite(IndicatorLED3, LOW);     //Disables LED Indicator 3
+    digitalWrite(laserEmitterPin, LOW);   //Disables Laser Emitter
   }
-  
-  digitalWrite(laserEmitterPin, HIGH);
+  else {
+    if (lives == 2) {
+      //2 Lives Remaining
+      digitalWrite(IndicatorLED3, LOW);   //Enable LED Indicator 3
+    }
+    else if (lives == 1) {
+      //1 Life Remaining
+      digitalWrite(IndicatorLED2, LOW);   //Disables LED Indicator 2
+      digitalWrite(IndicatorLED3, LOW);   //Disables LED Indicator 3
+    }
+
+    //Hit Detection
+    if (digitalRead(laserRecier) == true) {
+      //Hit Detected
+      lives = lives - 1;//Remove 1 Life
+    }
+    
+    digitalWrite(laserEmitterPin, HIGH);//Turn on the Laser
+  }
 }
