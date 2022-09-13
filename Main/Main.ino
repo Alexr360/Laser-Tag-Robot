484c43452f77900e06e68e1b0774b1addfd6006e
Alexr360 <70153720+Alexr360@users.noreply.github.com>
2022-09-13 12:52:58 -0600

    Updated Livevs System

--- Main/Main.ino
+++ Main/Main.ino
@@ -34,6 +34,7 @@ int IndicatorLED3 = 7;
 //Startup Values
 int startSteeringPos = 90; //Servo can be set from 0-180 so 90 is the middle
 int lives = 3; // Number of Lives at the start
+bool alive = false;
 
 void setup() {
   //Laser Setup
@@ -56,30 +57,45 @@ void setup() {
 }
 
 void loop() {
-  if (lives < 1) {
+  //Hit Detection
+  if (digitalRead(laserRecier) == true && alive == true) {
+    //Hit Detected
+    lives = lives - 1;//Remove 1 Life
+    alive = livesIndicator(lives);
+  }
+}
+
+bool livesIndicator(int lives) {
+  if (lives == 3) {
+    //3 lives 
+    digitalWrite(IndicatorLED1, HIGH);     //Enables LED Indicator 1
+    digitalWrite(IndicatorLED2, HIGH);     //Enables LED Indicator 2
+    digitalWrite(IndicatorLED3, HIGH);     //Enables LED Indicator 3
+    digitalWrite(laserEmitterPin, HIGH);   //Enables Laser Emitter
+    return:true;
+  }
+  else if (lives == 2) {
+    //2 Lives
+    digitalWrite(IndicatorLED1, HIGH);     //Enables LED Indicator 1
+    digitalWrite(IndicatorLED2, HIGH);     //Enables LED Indicator 2
+    digitalWrite(IndicatorLED3, LOW);      //Disables LED Indicator 3
+    digitalWrite(laserEmitterPin, HIGH);   //Enables Laser Emitter
+    return:true;
+  }
+  else if (lives == 1) {
+    //1 Life
+    digitalWrite(IndicatorLED1, HIGH);     //Enables LED Indicator 1
+    digitalWrite(IndicatorLED2, LOW);      //Disables LED Indicator 2
+    digitalWrite(IndicatorLED3, LOW);      //Disables LED Indicator 3
+    digitalWrite(laserEmitterPin, HIGH);   //Enables Laser Emitter
+    return:true;
+  }
+  else if (lives < 1) {
     //Robot is Dead
     digitalWrite(IndicatorLED1, LOW);     //Disables LED Indicator 1
     digitalWrite(IndicatorLED2, LOW);     //Disables LED Indicator 2
     digitalWrite(IndicatorLED3, LOW);     //Disables LED Indicator 3
     digitalWrite(laserEmitterPin, LOW);   //Disables Laser Emitter
-  }
-  else {
-    if (lives == 2) {
-      //2 Lives Remaining
-      digitalWrite(IndicatorLED3, LOW);   //Enable LED Indicator 3
-    }
-    else if (lives == 1) {
-      //1 Life Remaining
-      digitalWrite(IndicatorLED2, LOW);   //Disables LED Indicator 2
-      digitalWrite(IndicatorLED3, LOW);   //Disables LED Indicator 3
-    }
-
-    //Hit Detection
-    if (digitalRead(laserRecier) == true) {
-      //Hit Detected
-      lives = lives - 1;//Remove 1 Life
-    }
-    
-    digitalWrite(laserEmitterPin, HIGH);//Turn on the Laser
+    return:false;
   }
 }
